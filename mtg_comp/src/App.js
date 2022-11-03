a04582c5683a609b15d0d3c9f7ed04ab848d4377
Alvin Toch <Alvinstoch@live.com>
2022-11-03 08:03:16 -0700

    initial setup II

--- mtg_comp/src/App.js
+++ mtg_comp/src/App.js
@@ -1,7 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import{BASE_URL} from './globals'
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(()=>{
+    const getCards= async()=>{
+      const response= await axios.get(`${BASE_URL}/v1`)
+    }
+    getCards
+  },[])
   return (
     <div className="App">
       <header className="App-header">
